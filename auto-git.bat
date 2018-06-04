03200b9fa09c0c8180e9b4bc33149b973adeae81
herrj1 <herreraj2015@fau.edu>
2018-06-04 14:59:13 -0400

    Added new code 06042018_145913

---
+++ auto-git.bat
@@ -0,0 +1,32 @@
+::!/bin/sh
+:: Author: Jonathan Herrera
+:: Description: Auto commit tool every 180 seconds. 
+:: License: MIT License 
+:: Copyright 2010-2018 Jonathan Herrera
+::###########################
+@echo off
+set UserInputPath=%cd%
+echo Current directory: %UserInputPath%
+cd %UserInputPath%
+:loop
+::set /p UserInputPath=Enter your project directory path?
+
+::# switch to branch you want to use
+::git checkout develop
+::# add all added/modified files
+git add .
+git ignore auto-git.bat
+::# commit changes
+::set /p CommitMessage=What would you like to say on your commit?
+::read commitMessage
+set Timestamp=%date:~4,2%%date:~7,2%%date:~10,4%_%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m "Added new code %Timestamp%"
+::# push to git remote repository
+git push origin master
+::###########################
+::echo %CommitMessage% 
+timeout /t 180
+goto loop
+echo Press Enter...
+
+::read
